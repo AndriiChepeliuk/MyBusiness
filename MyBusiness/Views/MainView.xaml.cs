f59b16c5bc85767047a0c32af672e0820c1339df
Andrii <andrewchepeliuk@gmail.com>
2023-01-31 15:43:55 +0200

    Added styles

--- MyBusiness/Views/MainView.xaml.cs
+++ MyBusiness/Views/MainView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace MyBusiness.Views
 {
@@ -10,6 +13,21 @@ namespace MyBusiness.Views
         public MainView()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
         }
     }
 }
