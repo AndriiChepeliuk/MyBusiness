6ac5c09b167e867c95bcca4906765b486dae75a1
Andrii <andrewchepeliuk@gmail.com>
2023-01-31 15:59:37 +0200

    Added control buttons

--- MyBusiness/Views/MainView.xaml.cs
+++ MyBusiness/Views/MainView.xaml.cs
@@ -29,5 +29,27 @@ namespace MyBusiness.Views
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
             this.MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
     }
 }
