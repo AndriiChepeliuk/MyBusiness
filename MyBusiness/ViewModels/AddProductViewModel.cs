9435962a58c7a3135dba983f4e55978a6ebcfb2f
Andrii <andrewchepeliuk@gmail.com>
2023-02-07 15:44:58 +0200

    Savepoint

--- MyBusiness/ViewModels/AddProductViewModel.cs
+++ MyBusiness/ViewModels/AddProductViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace MyBusiness.ViewModels
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Input;
+
+namespace MyBusiness.ViewModels
 {
-    internal class AddProductViewModel : ViewModelBase
+    public class AddProductViewModel : ViewModelBase
     {
+        public ICommand ChoosePictureCommand { get; }
+
+        public AddProductViewModel()
+        {
+            ChoosePictureCommand = new ViewModelCommand(ExecuteViewModelCommand);
+        }
+
+        private void ExecuteViewModelCommand(object obj)
+        {
+            OpenFileDialog open = new OpenFileDialog()
+            {
+                Title = "OpenOpen"
+            };
+
+            if (open.ShowDialog() == true)
+            {
+                StreamReader sr = new StreamReader(File.OpenRead(open.FileName));
+            }
+        }
     }
 }
--- MyBusiness/Views/AddProductView.xaml.cs
+++ MyBusiness/Views/AddProductView.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using MyBusiness.ViewModels;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MyBusiness.Views
 {
@@ -23,6 +14,25 @@ namespace MyBusiness.Views
         public AddProductView()
         {
             InitializeComponent();
+            DataContext = new AddProductViewModel();
+        }
+
+        private void addPicture_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void OpenFile()
+        {
+            OpenFileDialog open = new OpenFileDialog()
+            {
+                Title = "Open"
+            };
+
+            if (open.ShowDialog() == true)
+            {
+                StreamReader sr = new StreamReader(File.OpenRead(open.FileName));
+            }
         }
     }
 }
