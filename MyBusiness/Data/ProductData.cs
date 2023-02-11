457f07f2f6b2e82ea828290180f4f9bf0e2cdef1
Andrii <andrewchepeliuk@gmail.com>
2023-02-11 12:06:12 +0200

    commit

---
+++ MyBusiness/Data/ProductData.cs
@@ -0,0 +1,37 @@
+﻿using MyBusiness.Models.Product;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyBusiness.Data
+{
+    public class ProductData
+    {
+        public static void AddProduct(ProductModel product)
+        {
+            using (var context = new ApplicationContext())
+            {
+                var productToDB = new ProductModel();
+                context.Products.Add(productToDB);
+                context.SaveChanges();
+            }
+        }
+
+        public static List<ProductModel> GetAllProducts()
+        {
+            using (var context = new ApplicationContext())
+            {
+                var products = context.Products.OrderBy(p => p.Category).ToList();
+                return products;
+            }
+        }
+
+        public static ProductModel GetProductById(int id)
+        {
+            using (var context = new ApplicationContext())
+            {
+                var product = context.Products.FirstOrDefault(x => x.Id == id);
+                return product;
+            }
+        }
+    }
+}
--- MyBusiness/ViewModels/AllProductsViewModel.cs
+++ MyBusiness/ViewModels/AllProductsViewModel.cs
@@ -1,7 +1,31 @@
-﻿namespace MyBusiness.ViewModels
+﻿using MyBusiness.Data;
+using MyBusiness.Models.Product;
+using System.Collections.ObjectModel;
+
+namespace MyBusiness.ViewModels
 {
     public class AllProductsViewModel : ViewModelBase
     {
-        
+        private ObservableCollection<ProductModel> products;
+        public ObservableCollection<ProductModel> Products
+        {
+            get { return products; }
+            set
+            {
+                products = value;
+                OnPropertyChanged(nameof(Products));
+            }
+        }
+
+        public AllProductsViewModel()
+        {
+
+            Products = new ObservableCollection<ProductModel>(ProductData.GetAllProducts());
+
+
+
+        }
+
+
     }
 }
