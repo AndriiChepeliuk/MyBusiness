1ab35898a38396dedc6cc557016c19c9d985113e
Andrii <andrewchepeliuk@gmail.com>
2023-02-14 16:36:40 +0200

    Added ability to open new window for editing selected product, chose new image

--- MyBusiness/ViewModels/AllProductsViewModel.cs
+++ MyBusiness/ViewModels/AllProductsViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using MyBusiness.Data;
 using MyBusiness.Models.Product;
+using MyBusiness.Views;
+using System;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
@@ -7,7 +9,18 @@ namespace MyBusiness.ViewModels
 {
     public class AllProductsViewModel : ViewModelBase
     {
+        private ProductModel selectedProduct;
         private ObservableCollection<ProductModel> products;
+
+        public ProductModel SelectedProduct
+        {
+            get { return selectedProduct; }
+            set
+            {
+                selectedProduct = value;
+                OnPropertyChanged(nameof(SelectedProduct));
+            }
+        }
         public ObservableCollection<ProductModel> Products
         {
             get { return products; }
@@ -24,12 +37,19 @@ namespace MyBusiness.ViewModels
         public AllProductsViewModel()
         {
             Products = new ObservableCollection<ProductModel>(ProductData.GetAllProducts());
-            EditProductCommand = new ViewModelCommand(ExecuteEditProductCommand);
+            EditProductCommand = new ViewModelCommand(ExecuteEditProductCommand, CanExecuteEditProductCommand);
+        }
+
+        private bool CanExecuteEditProductCommand(object obj)
+        {
+            return selectedProduct != null;
         }
 
         private void ExecuteEditProductCommand(object obj)
         {
-            Product = (ProductModel)obj;
+            EditProductViewModel editProductViewModel = new EditProductViewModel() { ProductToEdit = selectedProduct };
+            var editWindow = new EditProductViewWindow() { DataContext = editProductViewModel };
+            editWindow.ShowDialog();
         }
     }
 }
---
+++ MyBusiness/ViewModels/EditProductViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using MyBusiness.Helpers;
+using MyBusiness.Models.Product;
+using System.IO;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MyBusiness.ViewModels
+{
+    public class EditProductViewModel : ViewModelBase
+    {
+        private ProductModel productToEdit;
+        public ProductModel ProductToEdit
+        {
+            get { return productToEdit; }
+            set
+            {
+                productToEdit = value;
+                OnPropertyChanged(nameof(ProductToEdit));
+            }
+        }
+
+        public ICommand ChoosePictureCommand { get; }
+
+        public EditProductViewModel()
+        {
+            ChoosePictureCommand = new ViewModelCommand(ExecuteChoosePictureCommand);
+        }
+        //public EditProductViewModel(ProductModel productModel)
+        //{
+        //    ProductToEdit = productModel;
+        //}
+
+        private void ExecuteChoosePictureCommand(object obj)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                ProductToEdit.ProductImage = ImageHelper.ConvertImageToByteArray(op.FileName);
+                MemoryStream memoryStream = new MemoryStream(ProductToEdit.ProductImage);
+                var newImage = new BitmapImage();
+                newImage.BeginInit();
+                newImage.StreamSource = memoryStream;
+                newImage.EndInit();
+                ProductToEdit.Image = newImage;
+            }
+        }
+    }
+}
