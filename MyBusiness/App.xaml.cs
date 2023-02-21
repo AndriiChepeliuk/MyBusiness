0878b10d82d73a1fb966e7c800d846a4a5aa896c
Andrii <andrewchepeliuk@gmail.com>
2023-02-21 15:02:34 +0200

    Added hosting

--- MyBusiness/App.xaml.cs
+++ MyBusiness/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyBusiness.Views;
 using System.Windows;
 
 namespace MyBusiness
@@ -13,5 +10,32 @@ namespace MyBusiness
     /// </summary>
     public partial class App : Application
     {
+        public static IHost? AppHost { get; private set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainView>();
+                })
+                .Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+
+            var startupForm = AppHost.Services.GetRequiredService<MainView>();
+            startupForm.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
     }
 }
