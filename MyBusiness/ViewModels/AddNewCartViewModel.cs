69464f4e8b04a71815ffdec72137b0f9b3764c84
Andrii <andrewchepeliuk@gmail.com>
2023-03-15 22:18:10 +0200

    Added button "Cancel"

---
+++ MyBusiness/ViewModels/AddNewCartViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using UmbrellaBiz.Models.Customer;
+
+namespace UmbrellaBiz.ViewModels
+{
+    class AddNewCartViewModel : ViewModelBase
+    {
+        private CustomerModel _customer;
+
+        public ICommand CancelAddNewCartCommand { get; }
+
+        public AddNewCartViewModel() : this(new CustomerModel()) { }
+        public AddNewCartViewModel(CustomerModel selectedCustomer)
+        {
+            _customer = selectedCustomer;
+            CancelAddNewCartCommand = new ViewModelCommand(ExecuteCancelAddNewCartCommand);
+        }
+
+        private void ExecuteCancelAddNewCartCommand(object obj)
+        {
+            var wind = (Window)obj;
+            wind.Close();
+        }
+    }
+}
--- MyBusiness/ViewModels/AllCustomersViewModel.cs
+++ MyBusiness/ViewModels/AllCustomersViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using UmbrellaBiz.Models.Customer;
 using UmbrellaBiz.Services;
+using UmbrellaBiz.Views;
 
 namespace UmbrellaBiz.ViewModels
 {
@@ -28,11 +31,24 @@ namespace UmbrellaBiz.ViewModels
             }
         }
 
+        public ICommand AddNewCartForSelectedCustomerCommand { get; }
+
         public AllCustomersViewModel()
         {
             Customers = new ObservableCollection<CustomerModel>(CustomerModelService.GetAllCustomers());
+            AddNewCartForSelectedCustomerCommand = new ViewModelCommand(ExecuteAddNewCartCommand, CanExecuteAddNewCartCommand);
         }
 
+        private bool CanExecuteAddNewCartCommand(object obj)
+        {
+            return SelectedCustomer != null;
+        }
 
+        private void ExecuteAddNewCartCommand(object obj)
+        {
+            var addNewCartViewModel = new AddNewCartViewModel(selectedCustomer);
+            var addNewCartWindow = new AddNewCartViewWindow() { DataContext = addNewCartViewModel };
+            addNewCartWindow.Show();
+        }
     }
 }
